f0bff51141b6b71362966ecde096a6b4245e01c4
wolverine5696 <3654@holbertonschool.com>
2022-01-28 12:49:51 -0800

    added answers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * prints all single digits in a base of 10
+ *
+ * Return:(0)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+
+	return (0);
+}
