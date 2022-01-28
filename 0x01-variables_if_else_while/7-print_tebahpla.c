f0c0320159540148df104c67708bedcf3cbff2d7
wolverine5696 <3654@holbertonschool.com>
2022-01-28 13:16:58 -0800

    added answers

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
