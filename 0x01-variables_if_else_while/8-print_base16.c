8843ac539989451471a48595814f17244ca4142b
wolverine5696 <3654@holbertonschool.com>
2022-01-28 13:40:20 -0800

    added answers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ *
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
