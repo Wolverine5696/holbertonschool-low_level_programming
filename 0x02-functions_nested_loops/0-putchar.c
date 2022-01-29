2716cb020fc7369c74706822aafba20d3d96e9ed
wolverine5696 <3654@holbertonschool.com>
2022-01-28 21:07:47 -0800

    added answeri

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - writing a program that prints putchar
+ *
+ * return 0
+ */
+int main(void)
+{
+	putchar(' ');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
