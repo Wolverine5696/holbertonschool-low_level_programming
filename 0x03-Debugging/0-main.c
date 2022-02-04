15240062c3f30ba7be3871fcb873a5a957f184c2
wolverine5696 <3654@holbertonschool.com>
2022-02-03 16:38:09 -0800

    added answers

--- 0x03-Debugging/0-main.c
+++ 0x03-Debugging/0-main.c
@@ -1,33 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
-/* more headers goes there */
+#include "main.h"
 
-/* betty style doc for function main goes there */
 /**
- * main - generates a random number  and assigns
- * it to the variable n
- *
- * Return: n and evaluates against logical operators
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = 98;
+	positive_or_negative(i);
 
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
-	{
-		printf("%d is positive\n", n);
-	}
 	return (0);
 }
