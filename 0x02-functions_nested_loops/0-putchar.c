25bafc08b63a78455270f51b05fa240aaea91600
wolverine5696 <3654@holbertonschool.com>
2022-02-06 16:31:09 -0800

    fixed answer

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <main.h>
 /**
  * main - writing a program that prints putchar
@@ -7,14 +6,14 @@
  */
 int main(void)
 {
-	putchar(' ');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-	return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+return (0);
 }
