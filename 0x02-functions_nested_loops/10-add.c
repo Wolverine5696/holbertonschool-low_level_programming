3d81c6f295ba1b0bf96a953b12fd4094aa222179
wolverine5696 <3654@holbertonschool.com>
2022-02-06 20:54:25 -0800

    adding answers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * add - entry point
+ *
+ * Description: Prints the alphabet with _putchar
+ * @a: variable to add
+ * @b: variable to add
+ * Return: void
+ */
+int add(int a, int b)
+
+{
+	int sum;
+	sum = a + b;
+
+	return (sum);
+}
