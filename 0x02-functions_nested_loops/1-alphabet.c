3511e9b0c3d8b978bf05b54affab0c8ba5c1c53b
wolverine5696 <3654@holbertonschool.com>
2022-02-06 17:08:54 -0800

    added answer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: void
+ */
+int main(void)
+{
+	char beta;																													for (beta = 'a'; beta <= 'z'; beta++)
+	{
+		_putchar(beta);													}
+		_putchar('\n');	
+}
