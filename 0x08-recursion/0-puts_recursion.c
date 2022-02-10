d399eca844b10bd25e2b61a887b2b5a70dc4db75
wolverine5696 <3654@holbertonschool.com>
2022-02-09 21:32:32 -0800

    fixed answer

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,12 +10,12 @@ nclude "main.h"
 
 void _puts_recursion(char *s)
 {
-		if (*s == '\0')
-		
-			_putchar('\n');
-		else
-		{
-			_putchar (*s);
-			_puts_recursion(++s);
-		}
+	if (*s == '\0')
+
+		_putchar('\n');
+	else
+	{
+		_putchar (*s);
+		_puts_recursion(++s);
+	}
 }
