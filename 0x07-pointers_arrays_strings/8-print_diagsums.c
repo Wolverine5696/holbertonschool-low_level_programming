e8fc082b4a4c69558dc3ce24b2e7773acfb0c4b4
wolverine5696 <3654@holbertonschool.com>
2022-02-09 19:06:38 -0800

    adding answers

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,34 @@
 #include "main.h"
 /**
- * print_diagsums:  
- *
+ * print_diagsums:
+ *	
  * Return: Always 0.
  */
-
 void print_diagsums(int *a, int size)
-{
-	  return;
+
+int main(void)													
+}
+	int *a, size, rows, columns, a[3][3], Sum = 0;
+	  
+	 	printf("\n Please Enter Number of rows and columns  :  ");
+		scanf("%d %d", &a, &size);
+			 
+		printf("\n Please Enter the Matrix Elements \n");
+		for(rows = 0; rows < i; rows++)
+		{
+			for(columns = 0;columns < j;columns++)
+		{
+			scanf("%d", &a[rows][columns]);
+		}	
+		}
+   	  
+ 		for(rows = 0; rows < i; rows++)
+	  	{
+			Sum = Sum + a[rows][rows];
+		}
+ 
+ 	printf("\n The Sum of Diagonal Elements of a Matrix =  %d", Sum );
+
+	return 0;
+
 }
