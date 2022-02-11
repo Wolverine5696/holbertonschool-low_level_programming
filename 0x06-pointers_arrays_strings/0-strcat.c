7b627e65231c600be83aebf626ea7cc434ca5b17
wolverine5696 <3654@holbertonschool.com>
2022-02-11 15:25:19 -0800

    fixing the code

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,45 @@
+#include "_strlen.c"
 #include "holberton.h"
+
 /**
- * _strcat - append a strin to another string
- * @src: source srting(s)
- * @dest: Destination String
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ *
  * Return: dest
  */
+
+
+
 char *_strcat(char *dest, char *src)
 {
-	int start = 0;
 
-	while (*dest)
+/*
+ * use _strlen function to get length of the dest string
+ *
+ *
+ */
+
+	int i = _strlen(dest);
+	int j = 0;
+
+/**
+ * concatenate strings
+ *
+ */
+
+	while (src[j] != '\0')
 	{
-		dest++;
-		start++;
-	}
-	while (*src)
-	{
-		src++;
-		*(dest++) = *(src++);
-		start++;
-	}
-	dest = dest - start;
-	return (dest);
+		dest[i] = src[j];
+		i++;
+		j++;
+												}
+/*
+ *
+ * end on null byte
+ *
+ */
+
+			dest[i] = '\0';
+			return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,7 +8,7 @@
  * Description: append a string to another
  * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n);
 {
 	int j = 0;
 	int i = 0;
