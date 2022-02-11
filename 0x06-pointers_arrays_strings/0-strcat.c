41c9fcf8acd92af2f4e573fe340923af7629fef1
wolverine5696 <3654@holbertonschool.com>
2022-02-11 10:10:41 -0800

    recoded answer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,44 @@
-#include "main.c"
-#include "stdio.h"
-#include "string.h"
+#include "holberton.h"
+#include "_strlen.c"
+
+
+/**
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ *
+ * Return: dest
+ */
+
+
 
 char *_strcat(char *dest, char *src)
 {
+
 /**
- * use _strlen function to get length of the dest string
+ * use _strlen function to get length ofidest string
  *
  *
  */
+
 	int i = _strlen(dest), j = 0;
 
-/*
- * concatenate strings together
+/**
+ * concatenate strings
  *
  */
 
 	while (src[j] != '\0')
-{
-	dest[i] = src[j];
-i++;
-j++;															}
-		dest[i] = '\0'
-
-			return (dest);
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+/**
+ *
+ * end on null byte
+ *
+ */
+	dest[i] = '\0';
+return (dest);
 }
