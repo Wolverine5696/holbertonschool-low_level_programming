f368bf0871cfecc4d300ecb92ac7958d967e5316
wolverine5696 <3654@holbertonschool.com>
2022-02-12 19:33:42 -0800

    added answer

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include"holberton.h"
+#include"stdio.h"
+
+/**
+* main capitalizes all words on a string
+*
+* return s
+*/
+char *cap_string(char *s)
+}
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+		if (i == 0)
+		{
+			if ((s[i] >= 'a' && s[i] <= 'z'))
+				s[i] = s[i] - 32;
+				continue;
+		}
+		if (s[i] == ' ')
+		{
+			s[i] = s[i] - 32
+			continue;
+		}
+	}
+	else
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = s[i] + 32;
+	}
+	return (s);
+}
