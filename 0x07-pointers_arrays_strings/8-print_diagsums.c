67692267051cdd31187153fa9592dc7fad62eb4c
wolverine5696 <3654@holbertonschool.com>
2022-02-12 21:39:57 -0800

    fixed answer

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,10 +1,31 @@
 #include "main.h"
 /**
  * print_diagsums:
- *	
+ *
  * Return: Always 0.
  */
 void print_diagsums(int *a, int size)
 {
-	return;
+	int l;
+	int y = 0;
+	int sumlr = 0;
+	int sumrl = 0;
+
+	for (l = 0; l < size; l++)
+	{
+		sumlr += a[y];
+		y++;
+		a = a + size;
+	}
+	a = a - (size * l);
+	printf("%d, ", sumlr);
+	y = size - 1;
+	for (l = 0; l < size; l++)
+	{
+		sumr1 +=  a[y];
+		y--;
+		a - a + size;
+	}
+	printf("%d\n", sumr1);
+return;
 }
