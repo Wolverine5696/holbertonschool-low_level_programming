69bd1263592f01a30e8f1a039c4481e3a252f679
wolverine5696 <3654@holbertonschool.com>
2022-02-15 12:51:53 -0800

    fixed files

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,26 +1,3 @@
-#include "holberton.h"
-/**
- * _strcat - appends src to the dest string
- * @dest: string to append by src
- * @src: string to append to dest
- *
- * Return: dest
- */
 char *_strcat(char *dest, char *src)
 {
-	int start = 0;
-
-	while (*dest)
-	{
-		dest++;
-		start++;
-	}
-	while (*src)
-	{
-		src++;
-		*(dest++) = *(src++);
-		start++;
-	}
-	dest = dest - start;
-	return (dest);
 }
