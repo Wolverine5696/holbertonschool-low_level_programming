02cd480506af4f4072a7d90e3678ed3d3719007c
wolverine5696 <3654@holbertonschool.com>
2022-02-14 21:40:19 -0800

    adding 5 str

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,1 +1,33 @@
-char *_strstr(char *haystack, char *needle);
+#include "main.h"
+/**
+ * _strstr - function start
+ * @haystack:input string
+ * @needle: search criteria
+ * Description:find substring in string
+ * Return:Result
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] == '\0')
+					break;
+				if (needle[j] == haystack[i + j])
+					continue;
+				break;
+			}
+				if (needle[j] == '\0')
+					{
+					haystack += i;
+					return (haystack);											    }											}
+	}													    return (0);
+}
