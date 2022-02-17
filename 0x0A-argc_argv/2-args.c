dcb8c3079ca3da64de3262efb65ec7e7eddada27
wolverine5696 <3654@holbertonschool.com>
2022-02-17 15:15:36 -0800

    deleted whitespace

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* main: prints all arguments 
+* main: prints all arguments
 * @argc: arguments passed into it
 * @argv: prints arguments in array on newline
 * Return: 0
 */
 int main(int argc, char *argv[])
 {
-	int i; 
+	int i;
 
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 
 	return (0);
-}	
+}
