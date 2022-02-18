861e234e09463ea605af320c5fc92de10c858fe8
wolverine5696 <3654@holbertonschool.com>
2022-02-17 21:14:32 -0800

    added answer

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - program that adds 2 numbers
+ * @argc: arguments added together to get answer
+ * @argv: string of arrays to arguments
+ *
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum;
+
+	if (argc == 1)
+	{/* if no answer present */
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (isdigit(argv[i][j]))
+			{
+				printf("error\n");
+				return (1);
+			}
+		}
+	}
+	sum = sum + atoi(argv[i]);
+}
