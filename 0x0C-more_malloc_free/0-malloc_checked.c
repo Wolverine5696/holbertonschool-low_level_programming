45d92c4c3d32c14ff9241eb9cb2988819a2feda1
wolverine5696 <3654@holbertonschool.com>
2022-02-21 12:35:24 -0800

    added answer

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <limits.h>
 /**
-* Main - allocates memory using malloc
+* Main - malloc_checked functions to malloc int b
 *@b:This is the length to allocate in the memory
 * *Return: Returns a pointer to the allocated memory if malloc fails
 *
@@ -13,6 +13,8 @@
 */
 void *malloc_checked(unsigned int b)
 {
+	int *p;
+
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
