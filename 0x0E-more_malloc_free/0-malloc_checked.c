dd9559951df8cfe4cc6bf225779d4f5209dedae4
wolverine5696 <3654@holbertonschool.com>
2022-02-21 12:16:53 -0800

    added answers

--- 0x0E-more_malloc_free/0-malloc_checked.c
+++ 0x0E-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 /**
- * Main - 
- *
- *
- *
- *
- */
-
-char *create_array(unsigned int size, char c)
+* Main - allocates memory using malloc
+*@b:This is the length to allocate in the memory
+* *Return: Returns a pointer to the allocated memory if malloc fails
+*
+*the malloc_checked function should cause normal process termination
+*
+*with a status value of 98
+*/
+void *malloc_checked(unsigned int b)
+{
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+return (p);
+}
