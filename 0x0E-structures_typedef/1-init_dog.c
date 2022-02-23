79e17b7fec0290a26ebf0c3d140ff48ffeb35aea
wolverine5696 <3654@holbertonschool.com>
2022-02-22 21:09:50 -0800

    adding 1st answer file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * init_dog - initialized a variable of type structure
+ * @d: pointer to address
+ * @name: dogs name
+ * @age: dogs age
+ * @owner: dog owners name
+ *
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
