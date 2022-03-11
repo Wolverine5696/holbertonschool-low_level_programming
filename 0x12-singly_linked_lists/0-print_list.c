d25debee6e5afa330d8157c823a8f453c1a9de63
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-10 22:27:02 -0800

    fixing error

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodecount = 0;
 
 	while (h != NULL)
 	{
@@ -19,8 +19,8 @@ size_t print_list(const list_t *h)
 			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		count++;
+		nodecount++;
 		h = h->next;
 	}
-return (count);
+return (nodecount);
 }
