7e9ad145a8784eb9572a5d999fa9267b2811097d
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-11 22:55:39 -0800

    adding changes to correct mistakes

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -1,36 +1,37 @@
 #include "main.h"
 #include <stdio.h>
+#include "_pow_recursions.c"
 /**
  * binary_to_uint - converts a binary to an unsigned int
  * @b: binary number to convert
  * Return: converted number, or 0 if b isn't binary or NULL
- *
- *
  */
-
 unsigned int binary_to_uint(const char *b)
-	unsigned int decimal = 0;
-	unsigned int red = 1, index = 0;
+{
+	unsigned int decimal = 0, x = 0, z;
 
 	if (b == NULL)
 		return (0);
-	for (index = 0; b[index] != '\0'; index++)
-	{
-		if (b[index] != '0' && b[index] != '1')
-			return (0);
-	}
-	for (index = index - 1; index >= 0; index--)
-	{
-		char currentCharacter;
 
-		currentCharacter = b[index];
-		if (currentCharacter == '1')
+	/* go to end of array */
+	for (z = 0; b[z] != '\0'; z++)
+	{
+	/* check if binary */
+	if (((b[z]) != '0') && ((b[z]) != '1') && ((b[z]) != '\0'))
+		return (0);
+	}
+	/* move back from NULL byte */
+	z = z - 1;
+	while (b[x] != '\0')
+	{
+		if (b[z - i] == '1')
 		{
-			decimal += red;
+			if (x == 0)
+				decimal = decimal + 1;
+			else
+				decimal = decimal + _pow_recursions(2, i);
 		}
-		red = red * 2;
-		if (index == 0)
-			break;
+		i++;
 	}
 	return (decimal);
 }
