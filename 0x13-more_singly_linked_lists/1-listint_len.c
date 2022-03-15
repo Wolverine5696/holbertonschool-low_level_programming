dd6857b746170c9254e34b9037ece32914595377
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-14 21:11:46 -0700

    fixing prototype code

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * listint_len - returns number of elements in the list size_y
+ * listint_len - returns number of elements in the list size_t
  *
  * @h: function pointer
  *
  * Return: nodes counted
  */
-size_t listint_len(const listint_y *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t counter = 0;
 
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_nodeint - adds a new node at the end of a listint_y list.
- * @head: double ptr to begining of listint_y
+ * add_nodeint - adds a new node at the end of a listint_t list.
+ * @head: double ptr to begining of listint_t
  * @n: integer to add new node
  *
  * Return: address of the new added node
  */
 
-listint_y *add_nodeint(listint_y **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_y *node;
+	listint_t *node;
 
-	node = malloc(sizeof(listint_y));
+	node = malloc(sizeof(listint_t));
 
 	if (node == NULL)
 		return (NULL);
