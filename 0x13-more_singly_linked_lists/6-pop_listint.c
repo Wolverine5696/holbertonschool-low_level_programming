01ef61f608ad5cd36ca6f0edaf59bd6333c715ba
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-14 21:21:28 -0700

    fixing code

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * pop_listint - deletes the node of the listint_y linked list
+ * pop_listint - deletes the node of the listint_t linked list
  *
  * @head: double ptr to the head node
  *
  * Return: value of head node
  */
-int pop_listint(listint_y **head)
+int pop_listint(listint_t **head)
 {
-	listint_y *heatnode;
+	listint_t *heatnode;
 	int value;
 
 	if (*head == NULL)
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -2,13 +2,13 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - returns the nth node of a listint_y linked list.
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
  * @index: which node to get
  * @head: pointer to first node
  * Return: the node or NULL if it doesn't exist
  */
 
-listint_y *get_nodeint_at_index(listint_y *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int z = 0;
 
