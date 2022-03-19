0163f27a07bb868ba2ca47d37083edb4b52acd84
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-18 22:02:36 -0700

    fixed code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,32 +10,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fd, length, printedChars;
 	char *buffer;
-	int lenRead;
-	int lenWrite;
 
 	if (filename == NULL || letters < 1)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 
-		if (fd == -1)
-			return (0);
+	if (fd == -1)
+		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 		return (0);
 
-	lenRead = read(fd, buffer, letters);
+	length = read(fd, buffer, letters);
 
-	lenWrite = write(STDOUT_FILENO, buffer, lenRead);
+	printedChars = write(STDOUT_FILENO, buffer, length);
 
 
-	if (lenRead == -1 || lenWrite == -1)
+	if (length == -1 || printedChars == -1)
 		return (0);
-	
+
 	free(buffer);
 	close(fd);
-	return (lenWrite);
+	return (printedChars);
 }
