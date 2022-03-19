67387beffaf2700138a7a75fbab26e36708eec3c
Wolverine5696 <gomichigan5696@gmail.com>
2022-03-18 21:43:34 -0700

    readding code

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,31 +11,23 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int ft;
-	int len;
+	int res;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
-
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
+	{
 		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
 
-	len = _strlen(text_content);
-
-	ft = write(fd, text_content, len);
-
-	if (fd == -1 || ft == -1)
+	res = write(fd, text_content, strlen(text_content));
+	if (res == -1)
+	{
 		return (-1);
+	}
 
-
-
-	close(fd);
 	return (1);
-
 }
