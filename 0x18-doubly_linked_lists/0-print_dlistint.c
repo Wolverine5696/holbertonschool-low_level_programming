1008a733771e97f53cd8d98a6f9f240481304f48
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-10 16:23:08 -0700

    fixed header file

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -10,10 +10,10 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int listcount = 0;
-		
+
 	if (h == NULL)
 		return (0);
-	
+
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
@@ -21,4 +21,4 @@ size_t print_dlistint(const dlistint_t *h)
 		listcount++;
 	}
 	return (listcount);
-}	
+}
