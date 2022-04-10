564aa13c5e33318c6c0c86a3c039b5e66295860b
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-10 15:34:06 -0700

    changed code

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -9,17 +9,15 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t listcount = 0;
-	const dlistint_t *razor = h;
-	
+	int listcount = 0;
+		
 	if (h == NULL)
 		return (0);
 	
-	while (razor != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", razor->n);
+		printf("%d\n", h->n);
 		listcount++;
-		razor = razor->next;
 	}
 	return (listcount);
 }	
