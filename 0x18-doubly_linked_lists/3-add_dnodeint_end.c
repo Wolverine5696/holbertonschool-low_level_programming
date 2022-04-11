aeed4abe21882c2f031851006512b6c32985d241
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-11 11:35:59 -0700

    adding code and answer

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * add_dnodeint_end - adds a node at the end of the list
+ * @n: input number to take the struct
+ * @head: double linked list
+ * Return: new node
+ *
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = (dlistint_t *) malloc(sizeof(dlistint_t));
+	dlistint_t *last = *head;
+
+	if (new_node == HULL)
+	{
+		free(new_node);
+		return (new_node);
+	}
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (*head);
+	}
+	while (last->next = new_node;
+		last = last->next;
+	last->next = new_node;
+	new_node->prev = last;
+	return (*head);
+}
