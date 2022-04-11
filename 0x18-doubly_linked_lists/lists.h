1239b4a9dc0e962119137186830580eda3482d64
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-10 17:04:48 -0700

    adding and correcting header struct

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -16,8 +16,8 @@
 typedef struct dlistint_s
 {
 	int n;
-	struct dlistint_t *prev;
-	struct dlistint_t *next;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
