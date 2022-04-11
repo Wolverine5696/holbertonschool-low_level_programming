20819e53d05d088ce55f05e19dc01e08cd90cad5
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-11 14:15:46 -0700

    adding answer

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -3,11 +3,22 @@
 #include <stdarg.h>
 
 /**
- *
- *
+ * free_dlistint - free the memory from the doubly linked list
+ * @head: this is the list
  *
  *
  *
  */
 
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current = head;
 
+	while (current != NULL)
+	{
+		dlistint_t *next = current->next;
+
+		free(current);
+		current = next;
+	}
+}
