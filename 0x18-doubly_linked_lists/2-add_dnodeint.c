92ba0fb3555af6793d8de98207234bcb19bf1242
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-10 19:26:42 -0700

    adding correction

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
 
-	if (new node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
 
