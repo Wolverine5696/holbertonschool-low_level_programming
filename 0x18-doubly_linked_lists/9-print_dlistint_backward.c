16690ea0c783dcb6f23c4477b46c79bf1e6c166b
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-11 21:25:14 -0700

    adding code

---
+++ 0x18-doubly_linked_lists/9-print_dlistint_backward.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "lists.h"
+
+/**
+ * print_dlistint_backward - list that prints list backwards
+ * @h: pointer to the list
+ * Return: number of nodes in the list
+ *
+ */
+
+size_t print_dlistint_backward(const dlistint_t *h)
+{
+	size_t listcount;
+	const dlistint_t *red = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (red->next)
+		red = red->next;
+
+	for (listcount = 0; red != NULL; red = red->prev)
+	{
+		printf("%d\n", red->);
+		listcount++;
+	}
+	return (listcount);
+}
