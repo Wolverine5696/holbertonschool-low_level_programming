50ad68b46f53f9e3260b76fcb60ecd48d2c2fb4c
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-12 02:54:08 -0700

    creating hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,12 +1,32 @@
 #ifndef HASH_TABLES_H
 #define HASH_TABLES_H
 
+/**
+ * struct_hash_node_s - node of a hash table
+ * @key: the key string
+ * @value: corresponding to a key
+ * @next: a pointer to the next node of the list
+ */
+
 typedef struct hash_node_s
 {
 	char *key;
 	char *value;
 	struct hash_node_s *next;
 } hash_node_t;
+
+/**
+ * struct_hash_table_s - hash table structure
+ *
+ * @size: size of the array
+ * @array: an array of this size
+ */
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
