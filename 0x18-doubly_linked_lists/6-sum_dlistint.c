cf676d60a3e063b214ce67b01593a9e0206f12de
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-11 21:04:01 -0700

    adding answers

---
+++ 0x18-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_dlistint - this function returns the sum of all the data
+ * @head: this is the dll
+ * Return: the total of the list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		sum += head->;
+		head = head->next;
+	}
+}
