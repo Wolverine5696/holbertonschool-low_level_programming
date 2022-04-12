de18f5b92d65f1521d6366797a7aaf67b86d3991
Wolverine5696 <gomichigan5696@gmail.com>
2022-04-12 03:27:06 -0700

    added code

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - starting the djb2 algorithm
+ * @str: string used to generate the value
+ *
+ * Return: hash value
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int x;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
